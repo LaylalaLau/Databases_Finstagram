9e8752713cdd56815cb42e09bf7e44803c7b29d9
Frankie <zz1763@nyu.edu>
2019-12-21 20:10:23 -0500

    2rd trial

--- extension.py
+++ extension.py
@@ -142,9 +142,9 @@ def home():
     cursor.close()
     return render_template('home.html', username=user, photos=photos,friendGroups = friendGroups,comments=data2)
 
-#@app.route('/post_page', methods=["GET", "POST"])
-#def post_page():
-#    
+@app.route('/post_page', methods=["GET", "POST"])
+def post_page():
+    
 @app.route('/post', methods=["GET", "POST"])
 def post():
     username = session['username']
